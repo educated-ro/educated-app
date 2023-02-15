f15336921745e8228e139648e8ab50dc26200f42
Sebastian Simionescu <contact@sebastian-simionescu.dev>
2023-02-16 01:50:00 +0200

    Add tabs to assessments page

--- src/components/modules/Assessment/AssessmentTabs/AssessmentsTabs.tsx
+++ src/components/modules/Assessment/AssessmentTabs/AssessmentsTabs.tsx
@@ -1,10 +1,15 @@
 'use client'
 
 import axios from 'axios'
-import { Box, Card, CardContent, CardHeader } from '@mui/material'
+import { Box, Card, CardContent, CardHeader, Typography } from '@mui/material'
 import { Assessment, AssessmentSession } from '@/types/assessment'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
+import { SyntheticEvent, useState } from 'react'
+import Tab from '@mui/material/Tab'
+import TabContext from '@mui/lab/TabContext'
+import TabList from '@mui/lab/TabList'
+import TabPanel from '@mui/lab/TabPanel'
 
 type AssessmentsTabsProps = {
   userId: string
@@ -14,6 +19,12 @@ type AssessmentsTabsProps = {
 
 export default function AssessmentsTabs({ userId, takenAssessments, assessments }: AssessmentsTabsProps) {
   const router = useRouter()
+
+  const [value, setValue] = useState('')
+
+  const handleChange = (event: SyntheticEvent, newValue: string) => {
+    setValue(newValue)
+  }
 
   const handleRowClick = (assessmentId: string) => {
     axios
@@ -27,16 +38,98 @@ export default function AssessmentsTabs({ userId, takenAssessments, assessments 
       })
   }
 
+  const filteredAssessment = takenAssessments.filter(({ status }) => status === value)
+
+  const tabs = [
+    {
+      name: 'New',
+      value: '',
+      count: assessments.length,
+    },
+    {
+      name: 'In progress',
+      value: 'in-progress',
+      count: takenAssessments.filter(({ status }) => status === 'in-progress').length,
+    },
+    {
+      name: 'Under evaluation',
+      value: 'under-evaluation',
+      count: takenAssessments.filter(({ status }) => status === 'under-evaluation').length,
+    },
+    {
+      name: 'Finished',
+      value: 'finished',
+      count: takenAssessments.filter(({ status }) => status === 'finished').length,
+    },
+  ]
+
   return (
     <Box sx={{ width: '100%' }}>
-      {takenAssessments.map(({ id, sessionId, assessment }) => (
-        <Link key={id} href={`/assessments/${sessionId}`}>
-          <Card sx={{ my: 1.5, p: 2, cursor: 'pointer' }}>
-            <CardHeader title={assessment.name} titleTypographyProps={{ variant: 'h5', sx: { fontSize: 17, fontWeight: 600 } }} />
-            <CardContent sx={{ color: '#8A98A9' }}>{assessment.metadata.description}</CardContent>
-          </Card>
-        </Link>
-      ))}
+      <Box sx={{ width: '100%', typography: 'body1', mb: 5 }}>
+        <TabContext value={value}>
+          <Box>
+            <TabList
+              onChange={handleChange}
+              aria-label='lab API tabs example'
+              TabIndicatorProps={{ sx: { display: 'none' } }}
+              sx={{
+                '& button': {
+                  fontWeight: 500,
+                  fontSize: 18,
+                  textTransform: 'capitalize',
+                  color: '#8A98A9',
+                },
+                '& button.Mui-selected': {
+                  color: '#2F3D4E',
+                },
+              }}
+            >
+              {tabs.map(tab => (
+                <Tab
+                  label={
+                    <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+                      <Typography sx={{ fontWeight: 500 }}>{tab.name}</Typography>
+                      <Typography
+                        sx={{
+                          backgroundColor: value === tab.value ? '#0868DC' : '#DEE1E4',
+                          color: value === tab.value ? '#fff' : '#8A98A9',
+                          borderRadius: '100%',
+                          width: 22,
+                          height: 22,
+                          fontSize: 12,
+                          textAlign: 'center',
+                          lineHeight: 1.9,
+                          fontWeight: 500,
+                        }}
+                      >
+                        {tab.count}
+                      </Typography>
+                    </Box>
+                  }
+                  value={tab.value}
+                  key={tab.value}
+                />
+              ))}
+            </TabList>
+          </Box>
+        </TabContext>
+      </Box>
+
+      {!value
+        ? assessments.map(assessment => (
+            <Card key={assessment.id} onClick={() => handleRowClick(assessment.id)} sx={{ cursor: 'pointer', my: 3 }}>
+              <CardHeader title={assessment.name} />
+              <CardContent>{assessment.metadata?.description}</CardContent>
+            </Card>
+          ))
+        : filteredAssessment.map(({ sessionId, assessment }) => (
+            <Link href={`/assessments/${sessionId}`} key={sessionId}>
+              <Card key={assessment.id} sx={{ my: 3 }}>
+                <CardHeader title={assessment.name} />
+                <CardContent>{assessment.metadata?.description}</CardContent>
+              </Card>
+            </Link>
+          ))}
     </Box>
   )
 }
